73854fad63e557e98945424ec3032b23b78302ce
Philipi Magalhães <philipimagalhaes@gmail.com>
2020-03-30 12:40:00 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,9 @@ const cors = require('cors');
 const port = 3333;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors)
 
 
 app.listen(port, function () {
