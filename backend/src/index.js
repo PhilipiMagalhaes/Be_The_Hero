46bdde66af11ffca68932cf0910b86c55b0e2998
Philipi Magalhães <philipimagalhaes@gmail.com>
2020-03-30 12:38:31 -0300

    Seconde commmit
    Backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 const port = 3333;
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors)
 
 
 app.listen(port, function () {
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
+//Routes ONGS
 routes.get('/ongs', OngController.index);
-
 routes.post('/ongs', OngController.create);
-routes.post('/incidents', IncidentController.create);
 
-module.exports = routes;
+//Routes Incidents
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+//Routes Sessions
+routes.post('/session', SessionController.create);
+
+module.exports = routes; 
